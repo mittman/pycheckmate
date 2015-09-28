de7a6311a72f4b116352411e402c50a8f6715fdc
mittman <mittman@users.noreply.github.com>
2015-09-27 18:04:40 -0700

    Interactive check occupied and fixed #2

--- Game.py
+++ Game.py
@@ -6,7 +6,7 @@ class Game:
 		mode = None
 		end = 0
 
-		print("> WOULD YOU LIKE TO PLAY A GAME?")
+		print("> SHALL WE PLAY A GAME?")
 		print("y) yes, start a new game")
 		print("n) no, this is a test")
 		mode = input("Select (y/N): ")
@@ -29,48 +29,56 @@ class Game:
 		return mode, end
 
 	def ask_piece(self, board, player_x, player_y, remain):
-		n = len(remain)
-		i = 1
-
 		print("> ADD PIECE TO BOARD?")
-		for p in remain:
-			print(str(i) + ") " + p)
-			i += 1
-		print(str(i) + ") nope")
 
-		n += 1
-		option = input("Select [1-" + str(n) + "]: ")
+		i = 0
+		for p in remain:
+			i += 1
+			print(str(i) + ") " + p)
+		print(str(i+1) + ") quit")
+
+		n = len(remain)
+		option = input("Select [1-" + str(n+1) + "]: ")
 		try:
 			option = int(option)
 		except ValueError: self.ask_piece(board, player_x, player_y, remain)
 
-		if option == n:
+		if option == n + 1:
 			pass
-		elif option < n and option > 0 and len(remain) > 1:
-			remain.pop(option-1)
+		elif option <= n and option > 0 and n > 1:
+			print("debug 1")
 			piece_name = remain[option-1]
 			self.insert_piece(board, piece_name, player_x, player_y)
+			remain.pop(option-1)
 			self.ask_piece(board, player_x, player_y, remain)
-		elif option < n and option > 0:
-			remain.pop(option-1)
+		elif option <= n and option > 0:
+			print("debug 2")
 			piece_name = remain[option-1]
 			self.insert_piece(board, piece_name, player_x, player_y)
+			remain.pop(option-1)
 		else:
 			print("Try again")
 			self.ask_piece(board, player_x, player_y, remain)
 
 	def insert_piece(self, board, piece_name, player_x, player_y):
 		print("> STARTING POSITION FOR " + piece_name + " ?")
-		moveH = input("Horizontal [1-8]: ")
-		moveV = input("Vertical [1-8]: ")
 
+		moveH = input("Horizontal [1-8]: ")
 		moveH = int(moveH)
+		while moveH < 1 or moveH > 8:
+			print("ERROR: expected [1-8]")
+			moveH = input("Horizontal [1-8]: ")
+			moveH = int(moveH)
+
+		moveV = input("Vertical [1-8]: ")
 		moveV = int(moveV)
-		if moveH < 1 or moveH > 8:
+		while moveV < 1 or moveV > 8:
 			print("ERROR: expected [1-8]")
-			exit(1)
-		if moveV < 1 or moveV > 8:
-			print("ERROR: expected [1-8]")
+			moveV = input("Vertical [1-8]: ")
+			moveV = int(moveV)
+
+		if board.state[moveH][moveV] != '*':
+			print("ERROR: space occupied")
 			exit(2)
 
 		if piece_name == "PlayerX King":
