7725ca6abbe042404a39eb2ca1584e9b004fd4c7
nikehat <nikehat@users.noreply.github.com>
2015-10-06 17:40:16 -0700

    Update Player.py

--- Player.py
+++ Player.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 class Player:
 	def __init__(self, player_id):
 		self.id = player_id
-		self.pieces = OrderedDict({})
+		self.pieces = OrderedDict()
 		self.turn = 0
 
 	def add_piece(self, piece):
